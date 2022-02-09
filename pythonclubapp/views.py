099bbaf71a52e33a2ff7c098f7271840425b5fb6
gina-noel <Gina.Noel@seattlecolleges.edu>
2022-02-09 12:49:34 -0700

    Assignment 5 - resources and part of Assignment 6 (not completed)

--- pythonclubapp/views.py
+++ pythonclubapp/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, Event, User
 
 # Create your views here.
 def index(request):
     return render(request, 'pythonclubapp/index.html')
 
+# all may not be a good idea if you have a lot! 
+def resources(request):
+    resource_list=Resource.objects.all() 
+    return render(request, 'pythonclubapp/resources.html', {'resource_list' : resource_list})
+
+def meetings(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'pythonclubapp/meetings.html', {'meeting_list':meeting_list})
     
+def meetingdetail(request, id):
+    meeting=get_object_or_404(Meeting, pk=id)
+    return render(request, 'pythonclubapp/meetingdetail.html', {'meeting' : meeting})
