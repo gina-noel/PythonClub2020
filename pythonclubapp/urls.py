708608541be3c044f443f17c8f363c4ff9e2b52f
gina-noel <Gina.Noel@seattlecolleges.edu>
2022-02-14 12:21:49 -0700

    update for meeting details

--- pythonclubapp/urls.py
+++ pythonclubapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('resources/', views.resources, name='resources'),
-    path('meetings/', views.meetings, name='meetings'),    
+    path('meetings/', views.meetings, name='meetings'),
+    path('meetingdetail/<int:id>', views.meetingdetail, name='detail'), 
 ]
--- pythonclubapp/views.py
+++ pythonclubapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event, User
+from django.urls import reverse_lazy
 
 # Create your views here.
 def index(request):
